1c65e77374e294e3fc290433e12093d0a67f2580
MohammadYSF <aa.yosefiyan7@gmail.com>
2023-04-07 01:56:56 +0430

    trying to fix cors stuff

--- react_client/src/Service/callApi.js
+++ react_client/src/Service/callApi.js
@@ -1,16 +1,14 @@
-const BASE_URL = "localhost:7243/api/";
+const BASE_URL = "127.0.0.1:7243/api/";
 async function callApi(url , method='GET',body=null,headers={}){
     const options = {
         method,
-        mode: "no-cors",
-        "Access-Controll-Allow-Origin":"*",
         headers : {
             'Content-Type':'application/json',
             ...headers
         }
     };
     if (body){options.body = JSON.stringify(body);}
-    const response = await fetch(`${BASE_URL}${url}` , options);
+    const response = await fetch(BASE_URL + url , options);
     const data = await response.json();
     return data;
 }
